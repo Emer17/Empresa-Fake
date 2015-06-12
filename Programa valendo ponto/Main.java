b01804f699ff3a432bac0f87971156d9330d423f
marcoputon <marcoputon@gmail.com>
2015-06-12 09:31:20 -0300

    Comparar.

---
+++ Programa valendo ponto/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//import java.math.*;
+
+class Main
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String s;
+		s = sc.nextLine();
+		int numeros[] = new int[1];
+		System.out.println(s);
+	}
+}
